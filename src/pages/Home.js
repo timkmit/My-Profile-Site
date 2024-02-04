9e6890cedab5ff6a875ac3d8358b034f732ed986
Timur <75565352+timkmit@users.noreply.github.com>
2024-02-04 13:45:08 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
                         <ul className="content-list">
                             <li className="content-list__item">
                                 <h2 className="title-2">Frontend</h2>
-                                <p>JavaScript, TypeScript, React, Redux Toolkit, HTML, CSS, Styled Components, Bootstrap, Material UI, NextJs</p>
+                                <p>JavaScript, TypeScript, React, Redux Toolkit, HTML, CSS, Styled Components, Bootstrap, Material UI</p>
                             </li>
                             <li className="content-list__item">
                                 <h2 className="title-2">Backend</h2>
