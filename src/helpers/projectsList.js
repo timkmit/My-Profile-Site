c51297040b49213d0b2f05c970e116ec43ae056c
devdvlive <thesocot@mail.ru>
2024-02-04 12:55:29 +0300

    fix: cleaned projectsList

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -4,19 +4,6 @@ import project01Big from "./../img/projects/01-big.jpg"
 import project02 from "./../img/projects/02.jpg"
 import project02Big from "./../img/projects/02-big.jpg"
 
-import project03 from "./../img/projects/03.jpg"
-import project03Big from "./../img/projects/03-big.jpg"
-
-import project04 from "./../img/projects/04.jpg"
-import project04Big from "./../img/projects/04-big.jpg"
-
-import project05 from "./../img/projects/05.jpg"
-import project05Big from "./../img/projects/05-big.jpg"
-
-import project06 from "./../img/projects/06.jpg"
-import project06Big from "./../img/projects/06-big.jpg"
-
-
 const projects = [
     {
         title: 'Just filling',
