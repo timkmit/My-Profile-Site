1cad15cef49242319bdc23561ba2420ba4526632
Timur <timengalytchev@mail.ru>
2024-08-11 21:56:24 +0300

    six info

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
           <div className="header__wrapper">
               <h1 className="header__title">
                   <strong>Hi, my name is <em>Timur</em></strong><br />
-                  a frontend developer
+                  a fullstack developer
               </h1>
               <div className="header__text">
                   <p>with passion for learning and creating.</p>
