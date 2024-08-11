36e975ae5b55130829266f0ff492260e6d4f864f
Timur <timengalytchev@mail.ru>
2024-08-11 19:07:01 +0300

    fix info

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -12,13 +12,13 @@ const Footer = () => {
           <div className="container">
               <div className="footer__wrapper">
                   <ul className="social">
-                      <li className="social__item"><a href="https://vk.com/timkmit"><img src={vk} alt="Link" /></a></li>
+                      <li className="social__item"><a href="https://vk.com/t1mkm1t"><img src={vk} alt="Link" /></a></li>
                       <li className="social__item"><a href="https://www.instagram.com/t1mkm1t/"><img src={instagram} alt="Link" /></a></li>
-                      <li className="social__item"><a href="#!"><img src={twitter} alt="Link" /></a></li>
+                      <li className="social__item"><a href="https://x.com/t1mkm1t"><img src={twitter} alt="Link" /></a></li>
                       <li className="social__item"><a href="https://github.com/timkmit"><img src={gitHub} alt="Link" /></a></li>
                   </ul>
                   <div className="copyright">
-                      <p>© 2023 </p>
+                      <p>© 2024 </p>
                   </div>
               </div>
           </div>
